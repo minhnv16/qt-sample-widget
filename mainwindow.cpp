4fff771a7ab97cc18f09f45cbcb377b26db1ee1e
minh nv <nguyenminhasp@gmail.com>
2021-10-03 11:06:51 +0700

    add message box

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,8 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
 
+#include <QMessageBox>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -13,3 +15,15 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_clicked()
+{
+    QMessageBox msgBox;
+    msgBox.setText("Hihi");
+    msgBox.exec();
+}
+
+void MainWindow::on_textEdit_textChanged()
+{
+
+}
